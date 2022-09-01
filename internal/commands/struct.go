a1e3f0c54a09f08af4e97d6609d56da9a2e9862e
Nils Ponsard <nilsponsard@gmail.com>
2022-09-01 09:12:29 +0200

    feat: Improve command handler (#1)

--- internal/commands/struct.go
+++ internal/commands/struct.go
@@ -1,7 +1,7 @@
 package commands
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/NilsPonsard/verbosity"
 	"github.com/bwmarrin/discordgo"
@@ -9,9 +9,11 @@ import (
 )
 
 type CommandsHandler struct {
-	summaries map[string]*common.CommandDescriptor
+	descriptors        map[string]*common.CommandDescriptor
+	registeredCommands []*discordgo.ApplicationCommand
 }
 
+// Create a new command handler with given command descriptors
 func New(summaries []common.CommandDescriptor) *CommandsHandler {
 	summariesMap := make(map[string]*common.CommandDescriptor)
 
@@ -20,46 +22,76 @@ func New(summaries []common.CommandDescriptor) *CommandsHandler {
 	}
 
 	return &CommandsHandler{
-		summaries: summariesMap,
+		descriptors: summariesMap,
 	}
 }
 
+// Register every commands to the session
 func (h *CommandsHandler) Register(session *discordgo.Session) (registeredCommands []*discordgo.ApplicationCommand) {
-	registeredCommands = make([]*discordgo.ApplicationCommand, len(h.summaries))
 
-	for _, summary := range h.summaries {
-		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, "", summary.Command)
+	for _, descriptor := range h.descriptors {
+		verbosity.Debug("Registering command:", descriptor.Command.Name)
+		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, "", descriptor.Command)
 		if err != nil {
 			verbosity.Fatal(err)
 
 		}
 		verbosity.Debug("Registered command:", cmd.Name)
 
-		registeredCommands = append(registeredCommands, cmd)
+		h.registeredCommands = append(h.registeredCommands, cmd)
 
 	}
 
 	return
 }
 
+// Handle a discord interaction event and dispatch them to the correspondin descriptor
+// InteractionApplicationCommand are found by name
+// InteractionMessageComponent are matched with the prefix of the custom id being the command name
 func (h *CommandsHandler) Handle(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
 
-	verbosity.Debug(interaction.Type)
-	verbosity.Debug(interaction.ID)
+	switch interaction.Type {
 
-	if interaction.Type == discordgo.InteractionMessageComponent {
+	// slash commands
+	case discordgo.InteractionApplicationCommand:
+		name := interaction.ApplicationCommandData().Name
 
-		verbosity.Debug(fmt.Sprintf("message : %+v", interaction.MessageComponentData()))
-		return
+		descriptor, ok := h.descriptors[name]
+
+		if !ok {
+			verbosity.Error("No command found for name:", name)
+			return
+		}
+
+		descriptor.Execute(session, interaction)
+
+	// interactions on a message
+	case discordgo.InteractionMessageComponent:
+
+		customId := interaction.MessageComponentData().CustomID
+
+		// search for the command corresponding to the custom id
+
+		for key, descriptor := range h.descriptors {
+			if strings.HasPrefix(customId, key) {
+				descriptor.Execute(session, interaction)
+				return
+			}
+		}
+
+	default:
+		verbosity.Debug("Unhandled interaction type:", interaction.Type)
 	}
 
-	name := interaction.ApplicationCommandData().Name
+}
 
-	summary, ok := h.summaries[name]
-	if !ok {
-		verbosity.Debug("Unknown command:", name)
-		return
+// Unregister every commands from the session
+func (h *CommandsHandler) Close(session *discordgo.Session) {
+
+	for _, cmd := range h.registeredCommands {
+		err := session.ApplicationCommandDelete(session.State.User.ID, "", cmd.ID)
+		if err != nil {
+			verbosity.Error(err)
+		}
 	}
-	summary.Execute(session, interaction)
-
 }
