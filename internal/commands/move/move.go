12cb5c403255ef8e27d27e5d545660a181d70653
Nils Ponsard <nilsponsard@gmail.com>
2022-09-01 17:50:50 +0200

    fix: value

--- internal/commands/move/move.go
+++ internal/commands/move/move.go
@@ -87,8 +87,8 @@ func execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	// get source and destination channels
 
 	options := i.ApplicationCommandData().Options
-	sourceChannelId := options[0].StringValue()
-	destinationChannelId := options[1].StringValue()
+	sourceChannelId := options[0].Value.(string)
+	destinationChannelId := options[1].Value.(string)
 
 	sameChannelError := IsSameChannels(sourceChannelId, destinationChannelId)
 	if sameChannelError != nil {
