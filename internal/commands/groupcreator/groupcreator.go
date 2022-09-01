fddeb72c502fc7f8e3777604618ed4759b5d6462
WoodenMaiden <yann.pomie@laposte.net>
2022-09-01 15:43:21 +0200

    fix: better code quality
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- internal/commands/groupcreator/groupcreator.go
+++ internal/commands/groupcreator/groupcreator.go
@@ -14,76 +14,82 @@ import (
 var command = &discordgo.ApplicationCommand{
 	Name:        "create-group",
 	Description: "Create a new group, optionnaly private",
-	Options:[]*discordgo.ApplicationCommandOption{
+	Options: []*discordgo.ApplicationCommandOption{
 		{
-			Type: discordgo.ApplicationCommandOptionString,
-			Name: "name",
-			Required: true,
+			Type:        discordgo.ApplicationCommandOptionString,
+			Name:        "name",
+			Required:    true,
 			Description: "Name of the group to be created",
 		},
 		{
-			Type: discordgo.ApplicationCommandOptionBoolean,
-			Name: "visibility",
-			Required: false,
+			Type:        discordgo.ApplicationCommandOptionString,
+			Name:        "description",
+			Required:    false,
+			Description: "Description of the group",
+			MaxLength:   200,
+		},
+		{
+			Type:        discordgo.ApplicationCommandOptionBoolean,
+			Name:        "visibility",
+			Required:    false,
 			Description: "Wether the group is private or not. A private group is joignable only when you are invited",
 		},
-	} ,
+	},
 }
 
 func contains(s []string, e string) bool {
-    for _, v := range s {
-        if v == e {
-            return true
-        }
-    }
-    return false
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
 }
 
-//This command will store The User, give him a role that grants him acess to a fresh new channel
-func execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	var ROLE_PERMISSIONS int64 =
-		0x0000000000000040 | // ADD_REACTIONS
-		0x0000000000000200 | // STREAM
-		0x0000000000000400 | // VIEW_CHANNEL
-		0x0000000000000800 | // SEND_MESSAGES
-		0x0000000000004000 | // EMBED_LINKS
-		0x0000000000008000 | // ATTACH_FILES
-		0x0000000000010000 | // READ_MESSAGE_HISTORY
-		0x0000000000040000 | // USE_EXTERNAL_EMOJI
-		0x0000000000100000 | // CONNECT
-		0x0000000000200000 | // SPEAK
-		0x0000000002000000 | // USE_VAD
-		0x0000000004000000 | // CHANGE_NICKNAME
-		0x0000000800000000 | // CREATE_PUBLIC_THREADS
-		0x0000001000000000 | // CREATE_PRIVATE_THREADS
-		0x0000004000000000 | // SEND_MESSAGES_IN_THREADS
-		0x0000002000000000 	 // USE_EXTERNAL_STICKERS
-		
-	var ROLE_COLOR int = 3447003
-	var ROLE_HOIST bool = false
-	var ROLE_MENTIONNABLE bool = true
+func sendErrorMessage(s *discordgo.Session, i *discordgo.InteractionCreate, error string) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("❌ Error: %v", error),
+		},
+	})
+}
 
+var ROLE_PERMISSIONS int64 = 0x0000000000000040 | // ADD_REACTIONS
+	0x0000000000000200 | // STREAM
+	0x0000000000000400 | // VIEW_CHANNEL
+	0x0000000000000800 | // SEND_MESSAGES
+	0x0000000000004000 | // EMBED_LINKS
+	0x0000000000008000 | // ATTACH_FILES
+	0x0000000000010000 | // READ_MESSAGE_HISTORY
+	0x0000000000040000 | // USE_EXTERNAL_EMOJI
+	0x0000000000100000 | // CONNECT
+	0x0000000000200000 | // SPEAK
+	0x0000000002000000 | // USE_VAD
+	0x0000000004000000 | // CHANGE_NICKNAME
+	0x0000000800000000 | // CREATE_PUBLIC_THREADS
+	0x0000001000000000 | // CREATE_PRIVATE_THREADS
+	0x0000004000000000 | // SEND_MESSAGES_IN_THREADS
+	0x0000002000000000 // USE_EXTERNAL_STICKERS
+var ROLE_COLOR int = 3447003
+var ROLE_HOIST bool = false
+var ROLE_MENTIONNABLE bool = true
+
+// This command will store The User, give him a role that grants him acess to a fresh new channel
+func execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	member := i.Member
-
-	verbosity.Debug("User", member.User.ID, ",tries to create group '", i.ApplicationCommandData().Options[0].Value, "'")
 	groupName := strings.Replace(i.ApplicationCommandData().Options[0].StringValue(), " ", "-", -1)
 
 	guild, error := s.GuildChannels(i.GuildID)
-	if (error != nil) {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("❌ Error: %v", error),
-			},
-		})
-
-		return;
+	if error != nil {
+		sendErrorMessage(s, i, error.Error())
+		return
 	}
 
+	verbosity.Debug("User", member.User.ID, ", tries to create group '", i.ApplicationCommandData().Options[0].Value, "' in guild ", guild)
+
 	var groups []string
-	
-	verbosity.Debug("Guild: ", guild)
 
 	for i := 0; i < len(guild); i += 1 {
 		if guild[i].Type == discordgo.ChannelTypeGuildCategory {
@@ -92,43 +98,34 @@ func execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		}
 	}
 
-	verbosity.Debug(groups)
+	if contains(groups, groupName) {
+		sendErrorMessage(s, i, fmt.Sprintf("group %v already exists", groupName))
+		verbosity.Debug(fmt.Sprintf("group %v already exists", groupName))
 
-	if (contains(groups, groupName)) {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("❌ Error: group %v already exists", groupName),
-			},
-		})
-
-		return;
+		return
 	}
 
-
-	role, _ := s.GuildRoleCreate(i.GuildID, &discordgo.RoleParams {
-		Name: "grp-" + groupName,
-		Color: &ROLE_COLOR,
-		Hoist: &ROLE_HOIST, 
+	role, _ := s.GuildRoleCreate(i.GuildID, &discordgo.RoleParams{
+		Name:        "grp-" + groupName,
+		Color:       &ROLE_COLOR,
+		Hoist:       &ROLE_HOIST,
 		Permissions: &ROLE_PERMISSIONS,
 		Mentionable: &ROLE_MENTIONNABLE,
 	})
 
-
-
 	category, error := s.GuildChannelCreateComplex(
-		i.GuildID, 
+		i.GuildID,
 		discordgo.GuildChannelCreateData{
 			Name: groupName,
 			Type: discordgo.ChannelTypeGuildCategory,
 			PermissionOverwrites: []*discordgo.PermissionOverwrite{
 				{
-					ID: role.ID, 
-					Type: discordgo.PermissionOverwriteTypeRole,
+					ID:    role.ID,
+					Type:  discordgo.PermissionOverwriteTypeRole,
 					Allow: ROLE_PERMISSIONS,
 				},
 				{
-					ID: i.GuildID,
+					ID:   i.GuildID,
 					Type: discordgo.PermissionOverwriteTypeRole,
 					Deny: ROLE_PERMISSIONS,
 				},
@@ -136,49 +133,40 @@ func execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		},
 	)
 
-	if (error != nil) {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("%v", error),
-			},
-		})
-
-		return;
+	if error != nil {
+		sendErrorMessage(s, i, error.Error())
+		return
 	}
 
 	var wg sync.WaitGroup
 	wg.Add(4)
 
-	go func (){
+	go func() {
 		defer wg.Done()
-
 		s.GuildMemberRoleAdd(i.GuildID, member.User.ID, role.ID)
 	}()
 
 	// Creating text channel
-	go func () {
+	go func() {
 		defer wg.Done()
-
 		s.GuildChannelCreateComplex(
-			i.GuildID, 
+			i.GuildID,
 			discordgo.GuildChannelCreateData{
-				Name: "txt-" + groupName,
-				Type: discordgo.ChannelTypeGuildText,
+				Name:     "txt-" + groupName,
+				Type:     discordgo.ChannelTypeGuildText,
 				ParentID: category.ID,
 			},
 		)
 	}()
 
 	// Creating voice channel
-	go func () {
+	go func() {
 		defer wg.Done()
-
 		s.GuildChannelCreateComplex(
-			i.GuildID, 
+			i.GuildID,
 			discordgo.GuildChannelCreateData{
-				Name: "🔉voc-" + groupName,
-				Type: discordgo.ChannelTypeGuildVoice,
+				Name:     "🔉voc-" + groupName,
+				Type:     discordgo.ChannelTypeGuildVoice,
 				ParentID: category.ID,
 			},
 		)
@@ -191,12 +179,14 @@ func execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("✅ Created goup '%v'! 🎉🎉", groupName),
+				Content: fmt.Sprintf("✅ Created goup '%v'! 🎉", groupName),
 			},
 		})
 	}()
 
 	wg.Wait()
+
+	verbosity.Debug("User", member.User.ID, ", created group '", i.ApplicationCommandData().Options[0].Value, "' in guild ", guild)
 }
 
 var Summary = common.CommandDescriptor{
